5bcea8218c811b416d2e7ac8e95351c4adc9fc77
Alex <vbobiby.my@gmail.com>
2018-03-04 14:37:23 +0200

    Add dev files

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// document.querySelector('.title').addEventListener('click', e => {
+//   let title = e.target;
+//   let editInput = document.querySelector('.textfield');
+//   let card = e.target.parentElement;
+//   let isEditing = card.classList.contains('editing');
+
+//   if (isEditing) {
+//     title.textContent = editInput.value;
+//   } else {
+//     editInput.value = title.textContent;
+//   }
+
+//   card.classList.toggle('editing');
+// });
